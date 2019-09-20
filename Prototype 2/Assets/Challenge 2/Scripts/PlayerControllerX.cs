bfeaa8a8df4fd7727d2c120a5ccda06a0c74f9da
NicoleLawson <nicolemlawson1994@gmail.com>
2019-09-19 20:32:45 -0500

    completed the challenge

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Nicole Lawson
+ * PlayerControllerX
+ * Assignment 4
+ * Allows the player to spawn dogs
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; 
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= 1)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0;
+        }
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Nicole Lawson
+ * SpawnManagerX
+ * Assignment 4
+ * Randomly spawns balls
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        //randomly pick a ball
+        int rng = Random.Range(0, 3);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[rng], spawnPos, ballPrefabs[rng].transform.rotation);
+        spawnInterval = Random.Range(3, 6);
+    }
+
+}
