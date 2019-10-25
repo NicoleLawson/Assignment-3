11c2fe000a53e371aa6eb86d68be632fdd9d184a
NicoleLawson <nicolemlawson1994@gmail.com>
2019-10-25 15:48:32 -0500

    Completed the assignment

---
+++ Assignent 8/Assets/SampleScenes/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+    public GameObject SmallBox;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnBox", 2f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBox()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        float rndX = player.transform.position.x + Random.Range(-5f, 5f);
+        float rndZ = player.transform.position.z + Random.Range(-5f, 5f);
+        float rndY = player.transform.position.y + 5;
+        Vector3 dest = new Vector3(rndX, rndY, rndZ);
+        Instantiate(SmallBox, dest, Quaternion.identity );
+    }
+
+    public void CancelBoxInvoke()
+    {
+        CancelInvoke("SpawnBox");
+    }
+}
---
+++ Assignent 8/Assets/SampleScenes/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public GameObject canvas;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Sets the world space canvas to active
+    public void DisplayPrompt()
+    {
+        canvas.SetActive(true);
+        //Start coroutine that deactives the canvas after 5 seconds
+        StartCoroutine(DisplayCountdown());
+    }
+
+    IEnumerator DisplayCountdown()
+    {
+        yield return new WaitForSeconds(5);
+        canvas.SetActive(false);
+    }
+}
