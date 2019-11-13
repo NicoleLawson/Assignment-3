3be1ef10b9a0d86e1e7d1881671b70379ac344f1
NicoleLawson <nicolemlawson1994@gmail.com>
2019-11-12 20:12:33 -0600

    Finished the first page of the assignment

---
+++ Assignment10/Assets/CollectibleSpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleSpawnManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> collectibles;
+    [SerializeField] private List<GameObject> multicolorCollectibles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Alpha1") == true)
+        {
+            for(int index = 0; index <=3; index++)
+            {
+                int rnd = Random.Range(0, 3);
+                GameObject itemToPaint = collectibles[rnd];
+                Instantiate(itemToPaint);
+            }
+        }
+    }
+}
